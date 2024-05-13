f7b02540dcf445e7a1062c4f24f7c36823e701a3
Monica <monica@DESKTOP-AK9SUM.localdomain>
2024-05-13 07:56:51 -0700

    stripe setup account with flask application

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+import os
+import stripe # type: ignore
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+stripe_keys = {
+        "secret_key": os.environ["sk_test_51PG1q5FormwXQMeHcdCUJ5DDiTBlElrG6Pc1Nr3kI1z6XATepF2eVvXfjQWt4Jea6GvaujpcXNYt7TdfZ4TWyNRj006aEHHgPV"],
+        "publishable_key": os.environ["pk_test_51PG1q5FormwXQMeHxbCHD6RehWNOTo1nt2k82ji2F7Cy9aAtS6WmcWTXxedfKFhvFJmBrXxxFqcXdcEJV4kJqhu6004xS7niXh"],
+}
+stripe.api_key = stripe_keys["secret_key"]
 
 @app.route('/')
 def checkout():
