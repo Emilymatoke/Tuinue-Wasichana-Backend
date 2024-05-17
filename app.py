13be15ee3d6df4dbd77885afca69c0768e5759c8
Monica <monica@DESKTOP-AK9SUM.localdomain>
2024-05-17 04:30:14 -0700

    succesfully integrated stripe in the application

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
-import os
-import stripe # type: ignore
 from flask import Flask, render_template, request
+import stripe 
+import os
+from dotenv import load_dotenv
+# import checkout.html
+load_dotenv()
+
 
 app = Flask(__name__)
+
 stripe_keys = {
-        "secret_key": os.environ["sk_test_51PG1q5FormwXQMeHcdCUJ5DDiTBlElrG6Pc1Nr3kI1z6XATepF2eVvXfjQWt4Jea6GvaujpcXNYt7TdfZ4TWyNRj006aEHHgPV"],
-        "publishable_key": os.environ["pk_test_51PG1q5FormwXQMeHxbCHD6RehWNOTo1nt2k82ji2F7Cy9aAtS6WmcWTXxedfKFhvFJmBrXxxFqcXdcEJV4kJqhu6004xS7niXh"],
-}
+        "secret_key": os.getenv("STRIPE_SECRET_KEY"),
+        "publishable_key": os.getenv("STRIPE_PUBLISHABLE_KEY"),
+    }
+
 stripe.api_key = stripe_keys["secret_key"]
 
 @app.route('/')
 def checkout():
-        return render_template('checkout.html')
+        return render_template('checkout.html',key=stripe_keys['publishable_key'])
+
+@app.route('/charge', methods=['POST'])
+def charge():
+        # Amount in cents
+        # amount = 1000
+
+        customer = stripe.Customer.create(
+            email='customer@example.com',
+            source=request.form['stripeToken']
+        )
+
+        charge = stripe.Charge.create(
+            customer=customer.id,
+            amount=amount,
+            currency='usd',
+            description='Flask Charge'
+        )
+
+        return render_template('charge.html', amount=amount)
+
 
 if __name__ == '__main__':
         app.run()
